5591c4b3e907d8f0ce8787b2738e9379d7e98577
Aaron Yu <ayybuddyy@gmail.com>
2021-03-02 10:06:30 -0500

    Clean up files, add header and logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='app__header'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png'
+          alt=''
+          className='app__headerImage'
+        />
+      </div>
+
+      <h1>Hello World</h1>
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
